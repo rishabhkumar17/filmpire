9fdcb76c65c5e86028cb6ccf4f2449c984aebe5f
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-02-24 07:56:29 +0530

    intro

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+// import { Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline />
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
