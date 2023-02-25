f2ec97b9fe6f878265b9caa152b3c99f12c901eb
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-02-25 23:54:41 +0530

    navBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,35 @@
+import {
+  AppBar,
+  IconButton,
+  Menu,
+  Toolbar,
+  useMediaQuery,
+} from '@mui/material';
 import React from 'react';
 
+import useStyles from './styles';
+
 function NavBar() {
-  return <div>NavBar</div>;
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width:600px)');
+
+  return (
+    <AppBar position="fixed">
+      <Toolbar className={classes.toolbar}>
+        {isMobile && (
+          <IconButton
+            color="inherit"
+            edge="start"
+            style={{ outline: 'none' }}
+            onClick={() => {}}
+            className={classes.menuButton}
+          >
+            <Menu />
+          </IconButton>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default NavBar;
