fcd7651dbb45ed2ce6fbccff66c37fa1523524d7
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-02-28 23:50:40 +0530

    SideBar List

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,3 +1,11 @@
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+} from '@mui/material';
 import { useTheme } from '@mui/styles';
 import React from 'react';
 import { Link } from 'react-router-dom';
@@ -11,18 +19,51 @@ const logo = {
     'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png',
 };
 
+const categories = [
+  { label: 'Popular', value: 'popular' },
+  { label: 'Top Rated', value: 'top_rated' },
+  { label: 'Upcoming', value: 'upcoming' },
+];
+
+const demoCategories = [
+  { label: 'Comedy', value: 'comedy' },
+  { label: 'Action', value: 'action' },
+  { label: 'Horror', value: 'horror' },
+  { label: 'Animation', value: 'animation' },
+];
+
 function SideBar({ setMobileOpen }) {
   const theme = useTheme();
   const classes = useStyles();
 
   return (
-    <Link to="/" className={classes.imageLink}>
-      <img
-        className={classes.image}
-        src={theme.palette.mode === 'light' ? logo.redLogo : logo.blueLogo}
-        alt="filmpire logo"
-      />
-    </Link>
+    <>
+      <Link to="/" className={classes.imageLink}>
+        <img
+          className={classes.image}
+          src={theme.palette.mode === 'light' ? logo.redLogo : logo.blueLogo}
+          alt="filmpire logo"
+        />
+      </Link>
+      <Divider />
+      <List>
+        <ListSubheader>Categories</ListSubheader>
+        {demoCategories.map(({ label, value }) => (
+          <Link key={value} className={classes.links} to="/">
+            <ListItem onClick={() => {}} button>
+              <ListItemIcon>
+                <img
+                  src={logo.redLogo}
+                  className={classes.genreImages}
+                  height={30}
+                />
+              </ListItemIcon>
+              <ListItemText primary={label} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+    </>
   );
 }
 export default SideBar;
