5a33752cbb12c446b0fa49a208908c6a8559cf25
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-03-02 22:31:20 +0530

    grow animation

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid, Grow, Typography } from '@mui/material';
 import React from 'react';
 
 import useStyles from './styles';
@@ -7,9 +7,11 @@ function Movie({ movie, i }) {
   const classes = useStyles();
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
-      <Typography className={classes.title} variant="h5">
-        {movie.title}
-      </Typography>
+      <Grow in key={i} timeout={(i + 1) * 250}>
+        <Typography className={classes.title} variant="h5">
+          {movie.title}
+        </Typography>
+      </Grow>
     </Grid>
   );
 }
