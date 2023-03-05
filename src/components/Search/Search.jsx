97240827f79757d946e6fbe0f7e63fd0507ac9b0
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-03-05 23:29:10 +0530

    searchIcon

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import { InputAdornment, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { Search as SearchIcon } from '@mui/icons-material';
+import useStyles from './styles';
 
 function Search() {
-  console.log();
-  return <div>Search</div>;
+  const classes = useStyles();
+  const [query, setQuery] = useState('');
+
+  const handleKeyPress = () => {};
+  return (
+    <div className={classes.searchContainer}>
+      <TextField
+        onKeyPress={handleKeyPress}
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        variant="standard"
+        InputProps={{
+          className: classes.input,
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+    </div>
+  );
 }
 
 export default Search;
