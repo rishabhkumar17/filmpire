ca6089778959820cc84fdd69d4c4bdd64528bee1
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-03-06 22:50:58 +0530

    axios get

---
+++ src/utils/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const moviesApi = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  params: {
+    api_key: process.env.REACT_APP_TMDB_KEY,
+  },
+});
+
+export const fetchToken = async () => {
+  try {
+    const { data } = await moviesApi.get('/authentication/token/new');
+  } catch (error) {}
+};
