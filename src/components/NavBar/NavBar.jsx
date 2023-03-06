c58a0357fdec3aed5735fde9fc9024b713332a55
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-03-06 23:49:44 +0530

    access username from redux state

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -32,8 +32,6 @@ function NavBar() {
   const theme = useTheme();
   const dispatch = useDispatch();
 
-  console.log(user);
-
   const token = localStorage.getItem('request_token');
   const sessionIdFromLocalStorage = localStorage.getItem('session_id');
 
