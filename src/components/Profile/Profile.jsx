610918da655f129ab68cf424415e21799fabd609
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-03-07 21:35:12 +0530

    logout button

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,3 +1,5 @@
+import { ExitToApp } from '@mui/icons-material';
+import { Box, Button, Typography } from '@mui/material';
 import React from 'react';
 import { useSelector } from 'react-redux';
 
@@ -6,7 +8,19 @@ import { userSelector } from '../../features/auth';
 function Profile() {
   const { user } = useSelector(userSelector);
 
-  return <div>Profile - {user.username}</div>;
+  const logout = () => {};
+  return (
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>
+          My Profile
+        </Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+    </Box>
+  );
 }
 
 export default Profile;
