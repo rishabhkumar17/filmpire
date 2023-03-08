d8a10df69f39982a3078442945c29b98d4737337
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-03-08 11:28:14 +0530

    movie-poster

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,9 +1,12 @@
-import { Box, CircularProgress } from '@mui/material';
+import { Box, CircularProgress, Grid } from '@mui/material';
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
+
+import useStyles from './styles';
 import { useGetMovieQuery } from '../../services/TMDB';
 
 function MovieInformation() {
+  const classes = useStyles();
   const { id } = useParams();
   const { data, isFetching, error } = useGetMovieQuery(id);
 
@@ -23,7 +26,18 @@ function MovieInformation() {
     );
   }
 
-  return <div>MovieInformation {id}</div>;
+  return (
+    <Grid container className={classes.containerSpaceAround}>
+      <Grid item sx={12} lg={4}>
+        <img
+          className={classes.poster}
+          src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
+          alt={data?.title}
+        />
+      </Grid>
+      <Grid item />
+    </Grid>
+  );
 }
 
 export default MovieInformation;
