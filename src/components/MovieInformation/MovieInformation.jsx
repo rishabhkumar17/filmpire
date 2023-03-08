419eba72b7d27fcf65d3bd0ad566df7f48507cf1
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-03-08 05:47:04 +0530

    error & isFetching

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,10 +1,27 @@
+import { Box, CircularProgress } from '@mui/material';
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useGetMovieQuery } from '../../services/TMDB';
 
 function MovieInformation() {
   const { id } = useParams();
   const { data, isFetching, error } = useGetMovieQuery(id);
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <CircularProgress size="8rem" />
+      </Box>
+    );
+  }
+
+  if (error) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Link to="/">Something has gone wrong - Go back</Link>
+      </Box>
+    );
+  }
 
   return <div>MovieInformation {id}</div>;
 }
